27a846177fc6e0bcbf81abf1130e653ae46138cf
Shandra Alejandro Uribe Tovar <shandra_a7x@hotmail.com>
2023-12-19 02:06:39 -0600

    GG

--- Main.js
+++ Main.js
@@ -1,3 +1,35 @@
+function () {
+    // Obtén referencias a los elementos del formulario y al resultado
+    const calculatorForm = document.getElementById('calculatorForm');
+    const montoTotalInput = document.getElementById('montoTotal');
+    const numeroCuotasInput = document.getElementById('numeroCuotas');
+    const tasaInteresInput = document.getElementById('tasaInteres');
+    const resultadoDiv = document.getElementById('resultado');
+
+    // Agrega un event listener al formulario para el evento 'submit'
+    calculatorForm.addEventListener('submit', function (event) {
+        // Evita el comportamiento predeterminado del formulario
+        event.preventDefault();
+
+        // Obtiene los valores de los campos de entrada
+        const montoTotal = parseFloat(montoTotalInput.value);
+        const numeroCuotas = parseInt(numeroCuotasInput.value);
+        const tasaInteres = parseFloat(tasaInteresInput.value);
+
+        // Valida los valores de entrada
+        if (isNaN(montoTotal) || isNaN(numeroCuotas) || isNaN(tasaInteres) || montoTotal <= 0 || numeroCuotas <= 0 || tasaInteres <= 0) {
+            alert("Por favor, ingrese valores válidos y mayores que cero en todos los campos.");
+            return;
+        }
+
+        // Realiza el cálculo llamando a la función calcularPagoCuotas
+        const cuotaMensual = calcularPagoCuotas(montoTotal, numeroCuotas, tasaInteres);
+
+        // Muestra el resultado en el elemento con id 'resultado'
+        resultadoDiv.textContent = `El pago mensual sería: $${cuotaMensual}`;
+    });
+};
+
 function calcularPagoCuotas(montoTotal, numeroCuotas, tasaInteres) {
     // Calcula el interés mensual
     const interesMensual = tasaInteres / 100 / 12;
@@ -8,34 +40,3 @@ function calcularPagoCuotas(montoTotal, numeroCuotas, tasaInteres) {
     // Retorna el resultado redondeado a dos decimales
     return cuotaMensual.toFixed(2);
 }
-
-let continuar;
-do {
-    // Prompts de usuario con validación de entrada
-    let montoTotal = parseFloat(prompt("Ingrese el monto total:"));
-    if (isNaN(montoTotal) || montoTotal <= 0) {
-        alert("Por favor, ingrese un monto total válido y mayor que cero.");
-        continue;
-    }
-
-    let numeroCuotas = parseInt(prompt("Ingrese el número de cuotas:"));
-    if (isNaN(numeroCuotas) || numeroCuotas <= 0) {
-        alert("Por favor, ingrese un número de cuotas válido y mayor que cero.");
-        continue;
-    }
-
-    let tasaInteres = parseFloat(prompt("Ingrese la tasa de interés anual (%):"));
-    if (isNaN(tasaInteres) || tasaInteres <= 0) {
-        alert("Por favor, ingrese una tasa de interés válida y mayor que cero.");
-        continue;
-    }
-    tasaInteres /= 100; // Convertir la tasa a decimal
-
-    const cuotaMensual = calcularPagoCuotas(montoTotal, numeroCuotas, tasaInteres);
-    console.log(`El pago mensual sería: $${cuotaMensual}`);
-    
-    alert(`El pago total es de $${montoTotal}. El pago mensual sería de: $${cuotaMensual} por ${numeroCuotas} meses`);
-    
-    // Continuar el ciclo con un while mientras el usuario no diga que no
-    continuar = prompt("¿Desea realizar otro cálculo? (Sí/No)").toLowerCase();
-} while (continuar === "si");
