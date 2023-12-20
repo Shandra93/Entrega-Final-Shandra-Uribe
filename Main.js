5e5a5dd5c1b744e49f830c14ec94dc5b5d18ca9f
Shandra Alejandro Uribe Tovar <shandra_a7x@hotmail.com>
2023-12-19 23:34:03 -0600

    Cambios para segundaPreEntrega

--- Main.js
+++ Main.js
@@ -1,42 +1,38 @@
-function () {
-    // Obtén referencias a los elementos del formulario y al resultado
-    const calculatorForm = document.getElementById('calculatorForm');
+const resultados = [];
+
+function calcularPagoCuotas() {
     const montoTotalInput = document.getElementById('montoTotal');
     const numeroCuotasInput = document.getElementById('numeroCuotas');
     const tasaInteresInput = document.getElementById('tasaInteres');
     const resultadoDiv = document.getElementById('resultado');
 
-    // Agrega un event listener al formulario para el evento 'submit'
-    calculatorForm.addEventListener('submit', function (event) {
-        // Evita el comportamiento predeterminado del formulario
-        event.preventDefault();
+    const montoTotal = parseFloat(montoTotalInput.value);
+    const numeroCuotas = parseInt(numeroCuotasInput.value);
+    const tasaInteres = parseFloat(tasaInteresInput.value);
 
-        // Obtiene los valores de los campos de entrada
-        const montoTotal = parseFloat(montoTotalInput.value);
-        const numeroCuotas = parseInt(numeroCuotasInput.value);
-        const tasaInteres = parseFloat(tasaInteresInput.value);
+    if (isNaN(montoTotal) || isNaN(numeroCuotas) || isNaN(tasaInteres) || montoTotal <= 0 || numeroCuotas <= 0 || tasaInteres <= 0) {
+        alert("Por favor, ingrese valores válidos y mayores que cero en todos los campos.");
+        return;
+    }
 
-        // Valida los valores de entrada
-        if (isNaN(montoTotal) || isNaN(numeroCuotas) || isNaN(tasaInteres) || montoTotal <= 0 || numeroCuotas <= 0 || tasaInteres <= 0) {
-            alert("Por favor, ingrese valores válidos y mayores que cero en todos los campos.");
-            return;
-        }
+    const cuotaMensual = calcularPagoMensual(montoTotal, numeroCuotas, tasaInteres);
 
-        // Realiza el cálculo llamando a la función calcularPagoCuotas
-        const cuotaMensual = calcularPagoCuotas(montoTotal, numeroCuotas, tasaInteres);
+    resultados.push(`El pago total sería: $${montoTotal.toFixed(2)}, por ${numeroCuotas} meses con un interes del ${tasaInteres}%`);
+    console.log(resultados);
 
-        // Muestra el resultado en el elemento con id 'resultado'
-        resultadoDiv.textContent = `El pago mensual sería: $${cuotaMensual}`;
-    });
-};
-
-function calcularPagoCuotas(montoTotal, numeroCuotas, tasaInteres) {
-    // Calcula el interés mensual
-    const interesMensual = tasaInteres / 100 / 12;
-
-    // Calcula la cuota mensual
-    const cuotaMensual = (montoTotal * interesMensual) / (1 - Math.pow(1 + interesMensual, -numeroCuotas));
-
-    // Retorna el resultado redondeado a dos decimales
-    return cuotaMensual.toFixed(2);
+    resultadoDiv.innerHTML = resultados.join('<br>');
+    
 }
+
+// Función para calcular la cuota mensual
+function calcularPagoMensual(montoTotal, numeroCuotas, tasaInteres) {
+    const interesMensual = tasaInteres / 100 / 12;
+    const cuotaMensual = (montoTotal * interesMensual) / (1 - Math.pow(1 + interesMensual, -numeroCuotas));
+    return cuotaMensual;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Agrega un event listener al botón de calcular
+    const calcularButton = document.getElementById('calcularButton');
+    calcularButton.addEventListener('click', calcularPagoCuotas);
+});
