2a7bb52f1106222a50909477849cafc444cba71d
Shandra Alejandro Uribe Tovar <shandra_a7x@hotmail.com>
2023-12-27 17:30:04 -0600

    Ajustes

--- Main.js
+++ Main.js
@@ -24,38 +24,49 @@ const calculadora = {
         this.calcularButton.addEventListener('click', this.calcularPagoCuotas.bind(this));
     },
     calcularPagoCuotas: function () {
-        // se incorpora forEach como método de array para trabajar 
-        // sobre el array inputs y actualizar los valores en el objeto 
-        // resultados en la función calcularPagoCuotas
+        const inputsConValor = this.inputs.filter(input => input.value.trim() !== '');
+
+        console.log('Inputs con valor:', inputsConValor);
+
+        if (inputsConValor.length !== this.inputs.length) {
+            alert('Por favor, complete todos los campos antes de calcular.');
+            return;
+        }
+
+        const sonTodosNumericos = this.inputs.every(input => !isNaN(parseFloat(input.value)));
+
+        console.log('Son todos numéricos:', sonTodosNumericos);
+
+        if (!sonTodosNumericos) {
+            alert('Por favor, ingrese valores numéricos en todos los campos.');
+            return;
+        }
+
         this.inputs.forEach((input, index) => {
             resultados[Object.keys(resultados)[index]] = parseFloat(input.value);
         });
 
-        if (this.isValidInput()) {
-            resultados.cuotaMensual = this.calcularPagoMensual(
+        console.log('Resultados después de la asignación:', resultados);
+
+        this.mostrarResultados();
+    },
+    mostrarResultados: function () {
+        const resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.innerHTML = `
+            <p>El pago mensual sería: $${this.calcularPagoMensual(
                 resultados.montoTotal,
                 resultados.numeroCuotas,
                 resultados.tasaInteres
-            );
-            this.mostrarResultados();
-        } else {
-            alert("Por favor, ingrese valores válidos y mayores que cero en todos los campos.");
-        }
-    },
-    isValidInput: function () {
-        return this.inputs.every(input => !isNaN(parseFloat(input.value)) && parseFloat(input.value) > 0);
-    },
-    mostrarResultados: function () {
-        this.resultadoDiv.innerHTML = `
-            <p>El pago mensual sería: $${resultados.cuotaMensual.toFixed(2)}, por ${
-            resultados.numeroCuotas
-        } meses con un interés anual del ${resultados.tasaInteres}%</p>
+            ).toFixed(2)}, por ${resultados.numeroCuotas} meses con un interés anual del ${resultados.tasaInteres}%</p>
         `;
     },
     calcularPagoMensual: function (montoTotal, numeroCuotas, tasaInteres) {
         const interesMensual = tasaInteres / 100 / 12;
         const cuotaMensual =
             (montoTotal * interesMensual) / (1 - Math.pow(1 + interesMensual, -numeroCuotas));
+
+        console.log('Cálculo de la cuota mensual:', cuotaMensual);
+
         return cuotaMensual;
     },
 };
