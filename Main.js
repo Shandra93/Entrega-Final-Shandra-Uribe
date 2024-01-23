9e39f2bbab5129d02d0fc4d76bfae6e40430ad29
Shandra Alejandro Uribe Tovar <shandra_a7x@hotmail.com>
2024-01-23 02:57:10 -0600

    ya listo

--- Main.js
+++ Main.js
@@ -4,25 +4,29 @@ document.addEventListener('DOMContentLoaded', function () {
             nombre: 'Television 55" - LG',
             imagen: './Assets/television.avif',
             precio: 1000,
-            moneda: 'USD'
+            moneda: 'USD',
+            cantidad: 1
         },
         {
             nombre: 'Laptop 13" - HP',
             imagen: './Assets/laptop.jpg',
             precio: 800,
-            moneda: 'USD'
+            moneda: 'USD',
+            cantidad: 1
         },
         {
             nombre: 'Iphone 15 Pro - Apple',
             imagen: './Assets/iphone.jpg',
             precio: 1200,
-            moneda: 'USD'
+            moneda: 'USD',
+            cantidad: 1
         },
         {
             nombre: 'MacBook Pro - Apple',
             imagen: './Assets/macbook.jpg',
             precio: 1500,
-            moneda: 'USD'
+            moneda: 'USD',
+            cantidad: 1
         }
     ];
 
@@ -30,35 +34,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const carrito = document.getElementById('productosCarrito');
     const calcularButton = document.getElementById('calcularButton');
     const agregarAlCarritoButton = document.getElementById('agregarAlCarritoButton');
+    const limpiarCacheButton = document.getElementById('limpiarCacheButton');
     const resultadoDiv = document.getElementById('resultado');
 
     let productosCarrito = JSON.parse(localStorage.getItem('productosCarrito')) || [];
 
-    renderizarCarrito();
+    limpiarCacheButton.style.display = productosCarrito.length > 0 ? 'block' : 'none';
 
-
-    if (productosCarrito.length > 0) {
-        console.log(`Se guardo correctamente el registro en local storage`);
-        console.log(productosCarrito);
-
-        calcularButton.disabled = false;
-    }
+    calcularButton.disabled = true;
 
     agregarAlCarritoButton.addEventListener('click', function () {
-    const productoSeleccionado = productos.find(producto => producto.nombre === getProductoSeleccionado());
+        const productoSeleccionado = productos.find(producto => producto.nombre === getProductoSeleccionado());
 
-    if (productoSeleccionado) {
-        if (!existeEnCarrito(productoSeleccionado.nombre)) {
-            productosCarrito.push(productoSeleccionado);
+        if (productoSeleccionado) {
+            const index = productosCarrito.findIndex(item => item.nombre === productoSeleccionado.nombre);
+
+            if (index !== -1) {
+                productosCarrito[index].cantidad++;
+            } else {
+                productosCarrito.push({ ...productoSeleccionado, cantidad: 1 });
+            }
 
             localStorage.setItem('productosCarrito', JSON.stringify(productosCarrito));
+            console.log('Se agregaron productos a la memoria')
+            console.log(productosCarrito)
 
             renderizarCarrito();
-
             calcularButton.disabled = false;
+
+            document.getElementById('resultado').removeAttribute('disabled');
+
+            limpiarCacheButton.style.display = 'block';
         }
-    }
-});
+    });
+
+    limpiarCacheButton.addEventListener('click', limpiarCacheLocal);
 
     catalogoProductos.addEventListener('click', function (event) {
         const productoSeleccionado = event.target.closest('.producto');
@@ -78,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const monedaSeleccionada = document.getElementById('moneda').value;
             const numeroCuotas = parseInt(document.getElementById('numeroCuotas').value, 10);
             const tasaInteres = parseFloat(document.getElementById('tasaInteres').value);
-    
+
             if (monedaSeleccionada !== 'USD') {
                 const conversionRate = obtenerTasaDeCambio(monedaSeleccionada);
                 const montoEnMoneda = montoTotalCarrito * conversionRate;
@@ -88,17 +98,17 @@ document.addEventListener('DOMContentLoaded', function () {
                 resultadoDiv.innerHTML = `Monto total en ${monedaSeleccionada}: ${montoTotalCarrito.toFixed(2)} ${monedaSeleccionada}`;
                 calcularCuotas(montoTotalCarrito, numeroCuotas, tasaInteres);
             }
-    
+
             mostrarBotonLimpiarCache();
         } else {
             resultadoDiv.innerHTML = 'No hay productos en el carrito para calcular el pago.';
+            document.getElementById('resultado').setAttribute('disabled', true);
         }
     });
-    
+
     function calcularMontoTotalCarrito() {
-        return productosCarrito.reduce((total, producto) => total + producto.precio, 0);
+        return productosCarrito.reduce((total, producto) => total + (producto.precio * producto.cantidad), 0);
     }
-    
 
     function mostrarBotonLimpiarCache() {
         const limpiarCacheButton = document.createElement('button');
@@ -149,7 +159,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 productosCarrito = [];
                 renderizarCarrito();
                 resultadoDiv.innerHTML = '';
-                calcularButton.disabled = true;
+                if (calcularButton) {
+                    calcularButton.disabled = true;
+                }
+
+                limpiarCacheButton.style.display = 'none';
+
+                if (productosCarrito.length > 0) {
+                    calcularButton.disabled = false;
+                    document.getElementById('resultado').removeAttribute('disabled');
+                } else {
+                    calcularButton.disabled = true;
+                    document.getElementById('resultado').setAttribute('disabled', true);
+                }
 
                 Swal.fire('Caché local limpiado con éxito.', '', 'success');
             }
@@ -172,7 +194,7 @@ document.addEventListener('DOMContentLoaded', function () {
             imagen.alt = producto.nombre;
 
             const parrafo = document.createElement('p');
-            parrafo.textContent = `${producto.nombre} - Precio: ${producto.precio}`;
+            parrafo.textContent = `${producto.nombre} - Precio: ${producto.precio} ${producto.moneda}`;
 
             nuevoProducto.appendChild(imagen);
             nuevoProducto.appendChild(parrafo);
@@ -186,10 +208,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
         productosCarrito.forEach(producto => {
             const nuevoElemento = document.createElement('p');
-            nuevoElemento.textContent = `${producto.nombre} - Precio: ${producto.precio} ${producto.moneda}`;
+            nuevoElemento.textContent = `${producto.nombre} - Precio: ${producto.precio} ${producto.moneda} x ${producto.cantidad}`;
             carrito.appendChild(nuevoElemento);
         });
     }
 
     renderizarCatalogo();
+    renderizarCarrito();
 });
